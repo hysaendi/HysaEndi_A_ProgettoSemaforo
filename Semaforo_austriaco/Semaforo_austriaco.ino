31bd63a1b7b79ec7d320f24716467bebdcf5f494
Endi <endi.watch@gmail.com>
2019-02-02 09:52:44 +0100

    02/02/2019 - Suddivisione in metodi

--- Semaforo_austriaco/Semaforo_austriaco.ino
+++ Semaforo_austriaco/Semaforo_austriaco.ino
@@ -1,17 +1,108 @@
+ //dichiarazione variabili con la quale richiamo i pin
+    int rosso1;
+    int giallo1;
+    int verde1; 
+    int rosso2;
+    int giallo2;
+    int verde2;
+    int numlampeggi;
+    int richiesta;
+    int durataTotale;
+    int durataGiallo;
+    int lassotempoVerde;
+
+void richiediDurataVerde ();
+        
 void setup() {
   // put your setup code here, to run once:
-    pinMode (13, OUTPUT);
-    pinMode (12,OUTPUT);
-    pinMode (11, OUTPUT);
-    pinMode (10, OUTPUT);
-}
 
+    rosso1 = 2;
+    giallo1= 3;
+    verde1= 4;
+    rosso2 = 5;
+    giallo2 = 6;
+    verde2 = 7;
+
+  
+    pinMode (rosso1, OUTPUT);
+    pinMode (giallo1, OUTPUT);
+    pinMode (verde1, OUTPUT);
+    pinMode (rosso2, OUTPUT);
+    pinMode (giallo2, OUTPUT);
+    pinMode (verde2, OUTPUT);
+
+    richiesta = 0;
+}
+   
 void loop() {
   // put your main code here, to run repeatedly:
-   digitalWrite (13,HIGH);
-   delay (500);
-   digitalWrite (13, LOW);
-   delay (500);
+    if (richiesta == 0)
+    {
+      richiediValori();
+    }
     
+
+    digitalWrite (rosso1, HIGH);
+    digitalWrite (verde2,HIGH);
     
+       
 }
+
+
+   void richiediValori ()
+   {
+    richiediDurataTotale();
+    richiediDurataGiallo();
+    richiediDurataVerde();
+    richiediLampeggiVerde();
+   }
+
+  void richiediLampeggiVerde()
+  {
+    Serial.print ("Quanti lampeggi verdi?");
+    while (Serial.available() == 0) {};
+    String input = Serial.readString();
+    numlampeggi = input.toInt(); 
+  }
+
+  void richiediDurataTotale()
+  {
+    Serial.print ("Quanto deve durare il semaforo?");
+    while (Serial.available() == 0) {};
+    String input = Serial.readString();
+    durataTotale = input.toInt();
+         
+  }
+
+  void richiediDurataGiallo ()
+  {
+    Serial.print ("Quanto deve durare il giallo?");
+    while (Serial.available() == 0) {};
+    String input = Serial.readString();
+    durataGiallo = input.toInt();  
+  }
+  
+  void richiediDurataVerde ()
+  {
+    Serial.print ("Quanto lasso di tempo vuoi che ci sia da un lampeggio all'altro?");
+    while (Serial.available() == 0) {};
+    String input = Serial.readString();
+    lassotempoVerde = input.toInt();
+        
+  }
+
+
+  void lampeggiaVerde (int n, int ritardo)
+  {
+    for (int i = 0; i<= n; i++)
+    {
+      digitalWrite (verde2,HIGH);
+      delay (ritardo);
+      digitalWrite (verde2,LOW);
+      delay (ritardo);
+    }
+  }
+
+
+
+
